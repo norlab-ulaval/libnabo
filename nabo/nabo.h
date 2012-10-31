b8a806032a001e74f9a116a6295acd7f1e03cae5
Stéphane Magnenat <stephane@magnenat.net>
2012-10-31 15:25:20 -0700

    Fixed bug in version define.

--- nabo/nabo.h
+++ nabo/nabo.h
@@ -213,7 +213,7 @@ namespace Nabo
 	//! version of the Nabo library as string
 	#define NABO_VERSION "1.0.1"
 	//! version of the Nabo library as an int
-	#define NABO_VERSION_INT "10001"
+	#define NABO_VERSION_INT 10001
 	
 	//! Parameter vector
 	struct Parameters: public std::map<std::string, boost::any>
