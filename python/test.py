9f1f819dd3ed97fc9b5e98e75b21e7763b4d9724
David Landry <davidlandry93@gmail.com>
2017-03-03 16:04:46 -0500

    Now link against appropriate boost-python library.

--- python/test.py
+++ python/test.py
@@ -2,11 +2,11 @@ from pynabo import *
 import numpy as np
 
 x = np.array([[0.,3.], [1,2], [4,5]])
-print x
+print(x)
 
 nns = NearestNeighbourSearch(x)
 q = np.array([[1.1, 2.]])
-print q
+print(q)
 
 res = nns.knn(q, 2, 0, SearchOptionFlags.ALLOW_SELF_MATCH)
 print res[0]
