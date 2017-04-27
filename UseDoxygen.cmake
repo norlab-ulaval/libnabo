008a03956f65f43737c3c55d520120f180611bb0
David Landry <davidlandry93@gmail.com>
2017-04-27 10:50:33 +0200

    Fixed tabs instead of spaces

--- UseDoxygen.cmake
+++ UseDoxygen.cmake
@@ -109,9 +109,9 @@ if(DOXYGEN_FOUND AND DOXYFILE_IN_FOUND)
 
 	configure_file(${DOXYFILE_IN} Doxyfile IMMEDIATE @ONLY)
 
-  if(NOT TARGET doc)
-    add_custom_target(doc)
-  endif()
+	if(NOT TARGET doc)
+		add_custom_target(doc)
+	endif()
 
 	add_dependencies(doc doxygen)
 endif()
