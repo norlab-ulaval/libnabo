4bbfee963f90df45a9b78ac24573032247930cb8
Yoshua Nava <yoshua.nava.chocron@gmail.com>
2020-11-20 07:28:32 +0100

    Address comments from pull request

--- cmake/CatkinBuild.cmake
+++ cmake/CatkinBuild.cmake
@@ -11,22 +11,18 @@ find_package(catkin REQUIRED)
 find_package(Eigen3 REQUIRED)
 find_package(Boost REQUIRED COMPONENTS python)
 
-find_package(OpenMP REQUIRED)
-if (OpenMP_FOUND)
-  add_compile_options("${OpenMP_CXX_FLAGS}")
-  add_definitions(-DHAVE_OPENMP=${OpenMP_FOUND})
+find_package(OpenMP)
+if (NOT OpenMP_FOUND)
+  message("OpenMP was not found. It is highly recommended to build libnabo with OpenMP support.")
 endif()
 
 # Catkin package macro
 catkin_package(
   INCLUDE_DIRS
-    nabo
     ${CMAKE_SOURCE_DIR}
     ${EIGEN3_INCLUDE_DIR}
   LIBRARIES
     nabo
-  DEPENDS
-    Boost
 )
 
 ########################
@@ -37,22 +33,29 @@ add_library(nabo
   ${NABO_SRC}
   ${NABO_HEADERS}
 )
-
-target_include_directories(nabo
-  PUBLIC
-    ${CMAKE_SOURCE_DIR}
-    ${CMAKE_SOURCE_DIR}/nabo
-  SYSTEM
-    ${EIGEN3_INCLUDE_DIR}
-    ${Boost_INCLUDE_DIRS}
-    ${OpenMP_CXX_INCLUDE_DIRS}
+target_include_directories(nabo PUBLIC
+  ${CMAKE_SOURCE_DIR}
+  ${CMAKE_SOURCE_DIR}/nabo
 )
-
-target_link_libraries(nabo
+target_include_directories(nabo SYSTEM PRIVATE
+  ${EIGEN3_INCLUDE_DIR}
+  ${Boost_INCLUDE_DIRS}
+  ${OpenMP_CXX_INCLUDE_DIRS}
+)
+target_link_libraries(nabo PRIVATE
   ${catkin_LIBRARIES}
   ${Boost_LIBRARIES}
   ${OpenMP_CXX_LIBRARIES}
 )
+target_compile_options(nabo PRIVATE
+  "${OpenMP_CXX_FLAGS}"
+)
+target_compile_definitions(nabo PRIVATE
+  -DHAVE_OPENMP=${OpenMP_FOUND}
+)
+
+# Python bindings
+add_subdirectory(python)
 
 #############
 ## Install ##
@@ -69,4 +72,7 @@ install(
   DIRECTORY
     ${CMAKE_SOURCE_DIR}/nabo/
   DESTINATION ${CATKIN_GLOBAL_INCLUDE_DESTINATION}/nabo
+  FILES_MATCHING
+    PATTERN "*.h"
+    PATTERN "*.hpp"
 )
